52d9c796f9bff876d078fff7d9b66e2b1cce2695
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2023-05-09 12:18:09 -0400

    add exports

--- client/_main.lua
+++ client/_main.lua
@@ -10,6 +10,7 @@ end
 local function teleportPlayer(coords, location)
     DoScreenFadeOut(1000)
     --playCutscene()
+    location = #coordsTable - location
     Wait(1000 * Config.MinTransportTime * location)
     SetEntityCoords(PlayerPedId(), coords.x, coords.y, coords.z, false, true, true, false)
     --playCutscene(true)
@@ -17,7 +18,10 @@ local function teleportPlayer(coords, location)
 end
 
 local function sortCoordsByDistance(coordsTable, playerCoords)
+    if type(playerCoords) == 'vector4' then playerCoords = vec3(playerCoords.x, playerCoords.y, playerCoords.z) end
     table.sort(coordsTable, function(a, b)
+        if type(b) == 'vector4' then b = vec3(b.x, b.y, b.z) end
+        if type(a) == 'vector4' then a = vec3(a.x, a.y, a.z) end
         return #(b - playerCoords) < #(a - playerCoords)
     end)
     return coordsTable
@@ -28,7 +32,7 @@ RegisterNetEvent('trainstation:openMenu', function(location)
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
     local stationCoords = Config.Stations[location].ticketBooth
-    if #(coords - stationCoords) > 5 then return end
+    if #(coords - vec3(stationCoords.x, stationCoords.y, stationCoords.z)) > 5 then return end
     local organizedCoords = sortCoordsByDistance(coordsTable, coords)
     local menu = {
         {
@@ -38,17 +42,17 @@ RegisterNetEvent('trainstation:openMenu', function(location)
     }
     for k,v in ipairs(organizedCoords) do
         for i,t in ipairs(Config.Stations) do
-            if v == t.ticketBooth then
+            if v == t.ticketBooth and i ~= location then
                 local price = math.ceil(Config.MaxTransportPrice / k)
                 menu[#menu+1] = {
-                    header = v.label,
+                    header = t.label,
                     text = 'Price: $'..price,
                     params = {
                         isAction = true,
                         event = function(data)
                             exports[Config.Core]:TriggerCallback('trainstation:purchaseTicket', function(cb)
                                 if not cb then return end
-                                teleportPlayer(data.toCoords)
+                                teleportPlayer(data.toCoords, data.location)
                             end, data)
                         end,
                         args = {
@@ -64,19 +68,28 @@ RegisterNetEvent('trainstation:openMenu', function(location)
     exports[Config.Menu]:openMenu(menu)
 end)
 
+
 CreateThread(function()
     for k, v in pairs(Config.Stations) do
-        coordsTable[k] = v.ticketBooth
         if Config.UseTarget then
 
-        else        
-            exports[Config.Core]:createPrompt('travel_prompt' ..k , v.ticketBooth, 0xF3830D8E, 'Buy Train Tickets', {
+        else
+            exports[Config.Core]:createPrompt('travel_prompt'.. k, vector3(v.ticketBooth.x, v.ticketBooth.y, v.ticketBooth.z), 0xF3830D8E, 'Buy Train Tickets', {
                 type = 'client',
                 event = 'trainstation:openMenu',
-                args = k
+                args = {k}
             })
-            exports[Config.Core]:CreateBlip('travel_blip' ..k, v.label, v.ticketBooth.x, v.ticketBooth.y, v.ticketBooth.z, Config.Blip.sprite, Config.Blip.scale, Config.Blip.rotation, Config.Blip.radius)
-            exports[Config.Core]:SpawnPed('travel_ped' .. k, Config.PedModel, v.pedCoords.x, v.pedCoords.y, v.pedCoords.z, v.pedCoords.w)
         end
+        exports[Config.Core]:CreateBlip('travel_blip'.. k, v.label, v.ticketBooth.x, v.ticketBooth.y, v.ticketBooth.z, -250506368, 0.5)
+        exports[Config.Core]:SpawnPed('travel_ped'.. k, Config.PedModel, v.pedCoords.x, v.pedCoords.y, v.pedCoords.z, v.pedCoords.w)
+        coordsTable[k] = v.ticketBooth
+    end
+end)
+
+AddEventHandler("onResourceStop", function(resourceName)
+    if resourceName ~= GetCurrentResourceName() then return end
+    for k, v in pairs(Config.Stations) do
+        exports[Config.Core]:DeleteBlip('travel_blip'.. k)
+        exports[Config.Core]:RemovePed('travel_ped'.. k)
     end
 end)
--- server/_main.lua
+++ server/_main.lua
@@ -1,6 +1,8 @@
 exports[Config.Core]:CreateCallback('trainstation:purchaseTicket', function(source, cb, data)
     local price = math.ceil(Config.MaxTransportPrice /data.location)
     local Player = exports[Config.Core]:GetPlayer(source)
-    if not Player.Functions.RemoveMoney('cash', price, 'Train Ride.') then cb(false) return end
+    if not Player.Functions.RemoveMoney('cash', price, 'Train Ride.') then
+        if not Player.Functions.RemoveMoney('bank', price, 'Train Ride.') then cb(false) return end
+    end
     cb(true)
 end)
